6ea2dbb7324e115005adbb31379ef32791004c18
augustlh <superhedman@gmail.com>
2026-02-25 15:18:04 +0100

    Initial progress on definition of the data model in regards to requests
    Co-authored-by: RockRottenSalad <adsw@itu.dk>
    Co-authored-by: tropaadigselv <frap@itu.dk>

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import asyncio
 
-from app.api import hello_world
+from app.api import hello_world, evaluate
 
 app = FastAPI()
 # Don't add endpionts in main directly
 # Always add endpoints via the router
 app.include_router(hello_world.router, prefix="/hello_world")
+app.include_router(evaluate.router, prefix="/evaluate")
 
 @app.get("/")
 def index() -> dict[str, str]:
