5b4bdd9cd0a1d19c99e559a693934d1c7340af5a
RockRottenSalad <adsw@itu.dk>
2026-02-26 17:42:05 +0100

    Add in-code documentation to the evaluator model

--- app/core/models/evaluationModel.py
+++ app/core/models/evaluationModel.py
@@ -5,10 +5,29 @@ from pydantic import BaseModel, ValidationError
 T = TypeVar("T", bound=BaseModel)
 
 class BaseEvaluator:
+    """
+    Bind(parse) a dict[str, Any] to a concrete evaluator config
+
+    Args:
+        config (dict[str, Any]): The config to be binded
+
+    Returns:
+        T | None: A bound config or None if the config was invalid
+    """
     @abstractmethod
     def bind(self, config: dict[str, Any]) -> T | None:
         pass
 
+    """
+    Evaluate an AI output using a specific config
+
+    Args:
+        output (str): The AI output to be evaluated
+        config (T): The config which the evaluation is based upon
+
+    Returns:
+        bool: Whether if the evaluation passed according to the config or failed
+    """
     @abstractmethod
     def evaluate(self, output: str, config: T) -> bool:
         pass
@@ -16,7 +35,6 @@ class BaseEvaluator:
 
 class LengthEvaluatorConfig(BaseModel):
     expected_length: int
-
 
 class LengthEvaluator(BaseEvaluator):
     def bind(self, config: dict[str, Any]) -> LengthEvaluatorConfig | None:
@@ -36,20 +54,26 @@ class EvaluationRequest(BaseModel):
 
 
 class EvaluationRegistry:
-   registry: dict[str, BaseEvaluator]
+    """
+    A registry system for storing and retrieving evaluators using unique ids
 
-   def __init__(self):
-        self.registry = {}
-
-   def get(self, id: str) -> BaseEvaluator | None:
-       if self.registry[id] is not None:
-           return self.registry[id]
-       else:
-           return None
-
-   def register(self, id: str, evaluator: BaseEvaluator) -> bool:
-       self.registry[id] = evaluator
-       return True
+    Attributes:
+        registry (dict[str, BaseEvaluator]): Dictionary which maps evaluator ID to an instance of that evaluator 
+    """
+    registry: dict[str, BaseEvaluator]
+ 
+    def __init__(self):
+         self.registry = {}
+ 
+    def get(self, id: str) -> BaseEvaluator | None:
+        if self.registry[id] is not None:
+            return self.registry[id]
+        else:
+            return None
+ 
+    def register(self, id: str, evaluator: BaseEvaluator) -> bool:
+        self.registry[id] = evaluator
+        return True
 
 
 registry = EvaluationRegistry()
