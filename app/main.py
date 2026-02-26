92f7903489c4594382c7bd9911a3b4cf19748e23
augustlh <superhedman@gmail.com>
2026-02-26 13:54:14 +0100

    Made current code adhere to ruff guidelines
    Co-authored-by: CharlottePlanteig <cpla@itu.dk>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import asyncio
 
 from app.api import hello_world
 
@@ -8,12 +7,15 @@ app = FastAPI()
 # Always add endpoints via the router
 app.include_router(hello_world.router, prefix="/hello_world")
 
+
 @app.get("/")
 def index() -> dict[str, str]:
     return {"Message": "Use /hello_world/"}
 
-async def main() -> None:
+
+def main() -> None:
     print("Hello from workflow-evaluation!")
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
