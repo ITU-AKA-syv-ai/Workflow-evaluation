ae47e0fa1afdd2e6d6fe42d396f297c123bee086
Ask Christensen <ask.christensen@gmail.com>
2026-03-04 20:18:30 +0100

    Added a test fixture that guarantees the docker image is created for the run test to function

---
+++ tests/docker/conftest.py
@@ -0,0 +1,11 @@
+﻿import pytest
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.wait_strategies import LogMessageWaitStrategy
+
+
+@pytest.fixture(scope="session")
+def container():
+    with DockerContainer("fastapi-app").with_exposed_ports(8000).waiting_for(
+        LogMessageWaitStrategy("Application startup complete") #looks for the logs you would normally see in your terminal
+    ) as container:
+        yield container
