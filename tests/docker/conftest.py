e79dcce8df0c6d79e0bd9d757f778f719c9f178f
Ask Christensen <ask.christensen@gmail.com>
2026-03-04 23:45:57 +0100

    Changed the functionality a bit after reading up on how docker testing should be structured generally. Now the test fixture throws an exception if we have not created

--- tests/docker/conftest.py
+++ tests/docker/conftest.py
@@ -2,10 +2,14 @@
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.wait_strategies import LogMessageWaitStrategy
 
+from testcontainers.core.exceptions import ContainerStartException
 
 @pytest.fixture(scope="session")
 def container():
-    with DockerContainer("fastapi-app").with_exposed_ports(8000).waiting_for(
-        LogMessageWaitStrategy("Application startup complete") #looks for the logs you would normally see in your terminal
-    ) as container:
-        yield container
+    try:
+        with DockerContainer("fastapi-app").with_exposed_ports(8000).waiting_for(
+            LogMessageWaitStrategy("Application startup complete")
+        ) as container:
+            yield container
+    except ContainerStartException:
+        pytest.fail("Image 'fastapi-app' not found locally. Run 'docker build -t fastapi-app .' first.")
