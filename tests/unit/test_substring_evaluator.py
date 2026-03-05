add7fd668cc6d754407bb35dad0d215499e6becc
Charlotte <cpla@itu.dk>
2026-03-05 15:57:14 +0100

    Add happy path test for SubstringEvaluator

--- tests/unit/test_substring_evaluator.py
+++ tests/unit/test_substring_evaluator.py
@@ -1,2 +1,14 @@
 from app.core.models.substring_evaluator import SubstringEvaluator, SubstringEvaluatorConfig
 
+# def test_bind() -> None:
+    # conf = {"substring": "test"}
+    # bound_conf = SubstringEvaluator().bind(conf)
+    # assert bound_conf is not None
+    # assert bound_conf.substring == "test"
+
+def test_evaluation_happypath() -> None:
+    input = "abc"
+    eval = SubstringEvaluator()
+    conf = SubstringEvaluatorConfig(substring="bc")
+    assert eval.evaluate(input, conf)
+
