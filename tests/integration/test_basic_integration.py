d20f901bab3555bfb3e7cefb14a563f50e854236
Charlotte <cpla@itu.dk>
2026-03-05 13:22:06 +0100

    Basic integration test
    Co-authored-by: Sara <salb@itu.dk>

---
+++ tests/integration/test_basic_integration.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+# HTTP request -> FastAPI endpoint -> service layer -> evaluator -> result -> HTTP response
+def test_basic_integration() -> None:
+    # Arrange (HTTP request)
+    client = TestClient(app)
+
+    request = {
+        "output": "Hello, World!",
+        "configs": [
+            {
+                "evaluator_id": "substring_evaluator",
+                "config": {"substring": "World"}
+            }
+        ]
+}
+
+# Act (send HTTP request)
+    response = client.post("/evaluate", json=request)
+
+# Assert (validate the HTTP response)
+
+    assert response.status_code == 200  # check returned status code
+    assert response.json() == {"results": [{"evaluator_id": "substring_evaluator", "passed": True, "error": None}]}
