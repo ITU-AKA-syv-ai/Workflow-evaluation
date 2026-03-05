77e20a82858d9b2cc845791f7c44ee8ab90c33f6
Ask Christensen <ask.christensen@gmail.com>
2026-03-05 12:28:52 +0100

    .

--- tests/docker/test_docker_run.py
+++ tests/docker/test_docker_run.py
@@ -3,7 +3,7 @@ import requests
 
 def test_docker_returns_200(container) -> None:
 
-    host = container.get_container_host_ip()  # usually local host  # noqa: E117
+    host = container.get_container_host_ip()  # usually local
     port = container.get_exposed_port(
         8000
     )  # testcontainer lets docker assign a random host port and it is then retrieved
