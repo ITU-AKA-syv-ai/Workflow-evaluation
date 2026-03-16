271572515b696b45f6fe4454db6e62986483b5eb
Nikolej <nivl@itu.dk>
2026-03-16 16:58:55 +0100

    Added docstrings to llm_judge.py.
    Renamed argument in _normalise_and_aggregate method.

--- app/core/models/llm_judge.py
+++ app/core/models/llm_judge.py
@@ -65,6 +65,16 @@ class LLMJudgeEvaluator(BaseEvaluator):
         return LLMJudgeConfig.model_json_schema()
 
     def bind(self, config: dict[str, Any]) -> LLMJudgeConfig | None:
+        """
+        Converts a configuration consisting of ... to an instance of LLMJudgeConfig with ...
+
+        Args:
+            config (dict[str, Any]): The config to bind to a LLMJudgeConfig.
+
+        Returns:
+            LLMJudgeConfig | None: Returns LLMJudgeConfig if config is correctly formatted, otherwise None is returned.
+        """
+
         try:
             bound = LLMJudgeConfig.model_validate(config)
         except ValidationError:
@@ -77,6 +87,17 @@ class LLMJudgeEvaluator(BaseEvaluator):
         return 1.0
 
     def _evaluate(self, output: str, config: LLMJudgeConfig) -> EvaluationResult:
+        """
+        Uses an LLM to judge the output of another LLM based on user defined criteria in a rubric, by constructing a prompt based on aforementioned.
+
+        Args:
+        	output (str): The AI output to be evaluated.
+            config (LengthEvaluatorConfig): The config which specifies the criteria the judge should use, collectively called the rubric, as well as the original user prompt.
+
+        Returns:
+        	EvaluationResult: Result which contains the evaluator_id, normalised score, and a reasoning. The reasoning field contains the scores and reasoning of each individual criterion in the rubric.
+        """
+
         user_prompt = f"""
 
         Now here are the inputs.
@@ -94,9 +115,6 @@ class LLMJudgeEvaluator(BaseEvaluator):
             return EvaluationResult(
                 evaluator_id="llm_judge",
                 reasoning=response,
-                # norm_score = (score-1) / 3
-                # agg_norm_score = sum(norm_scores) / norm_scores.count
-                # Where norm_score is the score of an individual criterion
                 normalised_score = _normalise_and_aggregate(response),
             )
         except Exception as e:
@@ -106,9 +124,18 @@ class LLMJudgeEvaluator(BaseEvaluator):
                 error=str(e),
             )
 
-def _normalise_and_aggregate(result: LLMResponse) -> float:
+def _normalise_and_aggregate(response: LLMResponse) -> float:
+    """
+    Normalises and aggregates the scors of each criterion in the rubric. The scale is converted from 1-4 to 0-1.
+
+    Args:
+    	response: (LLMResponse): The response from the LLM judge, containing the rubric scores.
+
+    Returns:
+    	float: The normalised and aggregated score for each criterion in the rubric.
+    """
     normalised_scores = []
-    for res in result.results:
+    for res in response.results:
         normalised = (res.score-1) / 3
         normalised_scores.append(normalised)
 
