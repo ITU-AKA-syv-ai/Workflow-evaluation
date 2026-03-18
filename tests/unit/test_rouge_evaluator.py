6bba19b40765bf0805dc4a9ae0780b762206354a
RockRottenSalad <adsw@itu.dk>
2026-03-18 16:00:34 +0100

    Add tests that check rouge gracefully handles empty strings

--- tests/unit/test_rouge_evaluator.py
+++ tests/unit/test_rouge_evaluator.py
@@ -21,6 +21,7 @@ def test_rouge_n_unigram() -> None:
     assert isclose(round(score.f1_score, 2), 0.55)
 
 
+# Score should be zero since there isn't a single overlapping unigram
 def test_rouge_n_unigram_long_example_bad_match() -> None:
     reference = "The missile knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't, or where it isn't from where it is (whichever is greater), it obtains a difference, or deviation. The guidance subsystem uses deviations to generate corrective commands to drive the missile from a position where it is to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position that it was, is now the position that it isn't. In the event that the position that it is in is not the position that it wasn't, the system has acquired a variation, the variation being the difference between where the missile is, and where it wasn't. If variation is considered to be a significant factor, it too may be corrected by the GEA. However, the missile must also know where it was. The missile guidance computer scenario works as follows. Because a variation has modified some of the information the missile has obtained, it is not sure just where it is. However, it is sure where it isn't, within reason, and it knows where it was. It now subtracts where it should be from where it wasn't, or vice-versa, and by differentiating this from the algebraic sum of where it shouldn't be, and where it was, it is able to obtain the deviation and its variation, which is called error."
     model_output = "No matches with reference exist here"
@@ -33,6 +34,7 @@ def test_rouge_n_unigram_long_example_bad_match() -> None:
     assert score.f1_score == 0
 
 
+# Score should be greater than zero since tehre is at least some overlapping unigrams
 def test_rouge_n_unigram_long_example_partial_match() -> None:
     reference = "The missile knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't, or where it isn't from where it is (whichever is greater), it obtains a difference, or deviation. The guidance subsystem uses deviations to generate corrective commands to drive the missile from a position where it is to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position that it was, is now the position that it isn't. In the event that the position that it is in is not the position that it wasn't, the system has acquired a variation, the variation being the difference between where the missile is, and where it wasn't. If variation is considered to be a significant factor, it too may be corrected by the GEA. However, the missile must also know where it was. The missile guidance computer scenario works as follows. Because a variation has modified some of the information the missile has obtained, it is not sure just where it is. However, it is sure where it isn't, within reason, and it knows where it was. It now subtracts where it should be from where it wasn't, or vice-versa, and by differentiating this from the algebraic sum of where it shouldn't be, and where it was, it is able to obtain the deviation and its variation, which is called error."
     model_output = "We have a missile somewhere and it knows where it is sometimes."
@@ -43,6 +45,32 @@ def test_rouge_n_unigram_long_example_partial_match() -> None:
     assert score.precision > 0
     assert score.recall > 0
     assert score.f1_score > 0
+
+
+# NOTE: This is NOT the evaluator itself, it doesn't indicate errors.
+# We are expecting zeros across all scores since there is no overlap between reference and the model output.
+def test_rouge_n_empty_ref() -> None:
+    reference = ""
+    model_output = "No reference given!"
+    n_gram = 2
+
+    score = rouge_n(model_output, reference, n_gram)
+
+    assert score.precision == 0
+    assert score.recall == 0
+    assert score.f1_score == 0
+
+
+def test_rouge_n_empty_output() -> None:
+    reference = "No model output given!"
+    model_output = ""
+    n_gram = 2
+
+    score = rouge_n(model_output, reference, n_gram)
+
+    assert score.precision == 0
+    assert score.recall == 0
+    assert score.f1_score == 0
 
 
 def test_rouge_n_bigram() -> None:
