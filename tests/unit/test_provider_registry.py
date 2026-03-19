5781ca57ecf8f583b62645f8e1b0ea3a47f8284c
Marie Johansen <majoh@itu.dk>
2026-03-19 14:30:56 +0100

    Add structure for test of provider_registry

---
+++ tests/unit/test_provider_registry.py
@@ -0,0 +1,14 @@
+﻿import pytest
+from tests.conftest import MockProvider
+
+from app.core.models.providers import (
+    provider_registry
+)
+
+from app.core.models.providers.provider_registry import register_provider
+
+def test_register_provider() -> None:
+    register_provider("azure")
+    print(provider_registry.get_available_providers())
+    assert "test" in provider_registry.get_available_providers()
+
