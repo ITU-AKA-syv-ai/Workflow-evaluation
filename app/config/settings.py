2542e667ae4806871e8f118ed4adec27c73e63f5
Frederik Petersen <frap@itu.dk>
2026-03-26 20:11:02 +0100

    changed settings back

--- app/config/settings.py
+++ app/config/settings.py
@@ -50,7 +50,7 @@ class Settings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        env_file="./../../.env",
+        env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter="_",
         env_nested_max_split=1,
