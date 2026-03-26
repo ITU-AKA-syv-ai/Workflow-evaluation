48a52421b3abb78722c97fd47e10aa62d8db76b5
Marie Johansen <majoh@itu.dk>
2026-03-26 14:26:05 +0100

    Draft database model

---
+++ app/model.py
@@ -0,0 +1,10 @@
+﻿from datetime import datetime
+import uuid
+from sqlmodel import SQLModel, Field, DateTime
+
+# Database Model
+class Results(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    created_at: DateTime = Field(default_factory=datetime.now)
+    request: str
+    result: str
