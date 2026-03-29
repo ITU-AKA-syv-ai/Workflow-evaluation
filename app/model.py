8221ca8f3d5de1d7520f3d60d1b95ed418745c31
Marie Johansen <majoh@itu.dk>
2026-03-29 16:11:59 +0200

    add database model

---
+++ app/model.py
@@ -0,0 +1,18 @@
+﻿from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import JSON, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+# Database Model
+class Base(DeclarativeBase):
+    pass
+
+
+class Results(Base):
+    __tablename__ = "results"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=False)
+    request: Mapped[dict] = mapped_column(JSON)
+    result: Mapped[dict] = mapped_column(JSON)
