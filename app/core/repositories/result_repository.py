c3574b2bff8a2b624e95123adc5e074eea6d651f
Charlotte <cpla@itu.dk>
2026-03-31 16:00:26 +0200

    Add method to get result by id from the database

--- app/core/repositories/result_repository.py
+++ app/core/repositories/result_repository.py
@@ -1,3 +1,7 @@
+import json
+from uuid import UUID
+
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.core.models.aggregated_result_entity import AggregatedResultEntity
@@ -17,7 +21,7 @@ class ResultRepository:
 
     def __init__(self, session: Session) -> None:
         """
-        Initialize the ResultRepository with a SQLAlchemy session.
+        Initialize the ResultRepository with the SQLAlchemy session.
 
         Args:
              session (Session): The SQLAlchemy session object.
@@ -40,3 +44,30 @@ class ResultRepository:
         )
         self.session.add(result)
         self.session.commit()
+
+    def get_result_by_id(self, result_id: UUID) -> AggregatedResultEntity:
+        """
+       Queries the database for a Result with the given ID.
+        If found, it converts the Result to an AggregatedResultEntity, serializing
+        the `request` and `result` fields to JSON strings.
+        If the result is not found, it raises an HTTPException.
+
+        Args:
+            result_id (UUID): The ID of the result to retrieve.
+
+        Returns:
+            AggregatedResultEntity: The AggregatedResultEntity object representing the result.
+
+        Raises:
+            HTTPException: If the result is not found, it raises an HTTPException with status code 404.
+        """
+        result = self.session.query(Result).filter(Result.id == result_id).first()
+        if result is None:
+            raise HTTPException(status_code=404, detail=f"Result {result_id} not found")
+
+        return AggregatedResultEntity(
+            request=json.dumps(result.request),
+            result=json.dumps(result.result),
+            id=result.id,
+            created_at=result.created_at,
+        )
