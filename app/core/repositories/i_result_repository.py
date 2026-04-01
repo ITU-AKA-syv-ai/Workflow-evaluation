e27edfa449857e3d78fb83dd9a1ecdcb786627ff
Charlotte <cpla@itu.dk>
2026-04-01 13:08:59 +0200

    Implement an interface for the result repository
    This is done such that the storage layer is abstracted for future swap.

---
+++ app/core/repositories/i_result_repository.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+from typing import List
+
+from app.core.models.aggregated_result_entity import AggregatedResultEntity
+
+
+class IResultRepository(ABC):
+
+    @abstractmethod
+    def insert(self, aggregated_result: AggregatedResultEntity) -> None:
+        """
+        Inserts an AggregatedResultEntity into the database.
+
+        Args:
+            aggregated_result (AggregatedResultEntity): The aggregated result entity object to be added to the database.
+        """
+        pass
+
+    @abstractmethod
+    def get_result_by_id(self, result_id: UUID) -> AggregatedResultEntity:
+        """
+        Retrieves a single result by its unique ID.
+
+        Args:
+            result_id (UUID): The unique identifier of the result to retrieve.
+
+        Returns:
+            AggregatedResultEntity: The result object.
+
+        Raises:
+            HTTPException: If the result is not found, it raises an HTTPException with status code 404.
+
+        """
+        pass
+
+    @abstractmethod
+    def get_recent_results(self, limit: int, offset: int) -> list[AggregatedResultEntity]:
+        """
+        Retrieves a list of recent results.
+        Args:
+            limit (int): The number of results to retrieve.
+            offset (int): The number of results to skip.
+
+        Returns: list[AggregatedResultEntity]: A list of result objects.
+                                               If no results are found, it returns an empty list.
+
+        """
+        pass
