44de12109125e8dee39618f0df07491738e0039b
Charlotte <cpla@itu.dk>
2026-04-01 21:23:26 +0200

    Add init test and insert test for SQLAlchemyResultRepository

---
+++ tests/integration/test_sqlalchemy_result_repository.py
@@ -0,0 +1,33 @@
+from app.core.repositories.sqlalchemy_result_repository import SQLAlchemyResultRepository
+from app.core.models.aggregated_result_entity import AggregatedResultEntity
+from app.model import Result
+
+def test_init(db_session):
+    repo = SQLAlchemyResultRepository(db_session)
+    assert repo.session == db_session
+
+def test_insert_works_correctly(db_session):
+    repo = SQLAlchemyResultRepository(db_session)
+    initial_count = db_session.query(Result).count()  # Check if the table is empty
+
+    repo.insert(AggregatedResultEntity(request="test", result="test"))
+    final_count = db_session.query(Result).count()
+    agg_result = db_session.query(Result).first()  # Since we just inserted one row, we can fetch the first
+
+    assert final_count == initial_count + 1
+    assert agg_result.request == "test"
+    assert agg_result.result == "test"
+
+def test_insert_with_multiple_rows_should_have_unique_id(db_session):
+    repo = SQLAlchemyResultRepository(db_session)
+    initial_count = db_session.query(Result).count()
+
+    for i in range(5):
+        repo.insert(AggregatedResultEntity(request=f"test{i}", result=f"test{i}"))
+
+    final_count = db_session.query(Result).count()
+    all_results = db_session.query(Result).all()
+    all_ids = {r.id for r in all_results}  # creates a set of all ids, which cannot have duplicates
+
+    assert final_count == initial_count + 5
+    assert len(all_ids) == final_count
