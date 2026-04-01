e229c03a7e7e86b42a11c0f40b97d8970fe41c2f
Charlotte <cpla@itu.dk>
2026-04-01 13:09:51 +0200

    Update id type to UUID and add default value to created_at in results table

--- app/alembic/versions/3ed9c3418547_create_results_table.py
+++ app/alembic/versions/3ed9c3418547_create_results_table.py
@@ -6,6 +6,8 @@ Create Date: 2026-03-27 19:48:27.943850
 
 """
 from collections.abc import Sequence
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 import sqlalchemy as sa
 from alembic import op
@@ -20,8 +22,8 @@ depends_on: str | Sequence[str] | None = None
 def upgrade() -> None:
     op.create_table(
         "results",
-        sa.Column("id", sa.Integer(), primary_key=True),
-        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
+        sa.Column("created_at", sa.DateTime(), nullable=False, server_default=sa.func.now()),
         sa.Column("request", sa.JSON),
         sa.Column("result", sa.JSON),
     )
