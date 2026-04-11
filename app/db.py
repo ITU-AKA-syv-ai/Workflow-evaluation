23b2a96b2507defd277987f148bd943f4f2ed027
Marie Johansen <majoh@itu.dk>
2026-04-11 13:54:04 +0200

    make get_engine()

--- app/db.py
+++ app/db.py
@@ -1,12 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session
 
 from app.config.settings import get_settings
 
-settings = get_settings()
 
-# Create the SQLAlchemy engine
-engine = create_engine(settings.db.sqlalchemy_database_uri)
+def get_engine() -> Engine:
+    settings = get_settings()
+    # Create the SQLAlchemy engine
+    return create_engine(settings.db.sqlalchemy_database_uri)
 
 
 def init_db(session: Session) -> None:
