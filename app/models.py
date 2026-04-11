4491cb562608e659854d923875fea25773678a5b
Marie Johansen <majoh@itu.dk>
2026-04-11 19:26:18 +0200

    Revert "migrate to use db servers time as default for created_at"
    This reverts commit 17f9d8889b3f761710153dfe611680f9028f2278.

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from uuid import UUID, uuid4
 
 from sqlalchemy import JSON, DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.sql import func
 
 
 # Database Model
@@ -14,6 +13,6 @@ class Base(DeclarativeBase):
 class Result(Base):
     __tablename__ = "results"
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=False)
     request: Mapped[dict] = mapped_column(JSON)
     result: Mapped[dict] = mapped_column(JSON)
