6072aab85cce030a5677cbfe23f9fed759fb141e
Frederik Petersen <frap@itu.dk>
2026-04-23 20:50:49 +0200

    fixed style for overview page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,30 @@ import "./index.css";
 import App from "./App.tsx";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { ThemeProvider } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useMediaQuery } from "@mui/material";
+import { getTheme } from "./styles/theme.tsx";
+
+function Root() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () => getTheme(prefersDarkMode ? "dark" : "light"),
+    [prefersDarkMode],
+  );
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <App />
-    </LocalizationProvider>
+    <Root />
   </StrictMode>,
 );
