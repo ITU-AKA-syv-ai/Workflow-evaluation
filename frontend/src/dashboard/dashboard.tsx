15ced7431fa32a5d7573bbeec1fbbf98cd765351
RockRottenSalad <adam19sw@gmail.com>
2026-04-24 22:01:11 +0200

    Add plot support for multiple evaluators

--- frontend/src/dashboard/dashboard.tsx
+++ frontend/src/dashboard/dashboard.tsx
@@ -1,92 +1,89 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import 'chart.js/auto';
-import { Line, Chart } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
-import type { DataEntry, Evaluator } from './data.js'
-import { toDataEntry, dateToFormattedString, toEvaluator } from './data.js'
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
-interface ChartDataProps {
-    entryData: DataEntry[]
+import dayjs from "dayjs";
+import { Dayjs } from "dayjs";
+
+import type { AggregatedResultEntity, Evaluator } from '../dto/dto.tsx'
+import { dateToFormattedString } from './data.tsx'
+
+interface ChartProps {
+    data: AggregatedResultEntity[]
+    evaluators: Evaluator[]
 }
+function Chart({data, evaluators}: ChartProps) {
+//    const ref = useRef<ChartJS>(null);
 
-function ChartData({entryData}: ChartDataProps) {
-  const data = {
-    labels: entryData.map(a => dateToFormattedString(a.timestamp)),
-    datasets: [
-      {
-        label: "Evaluation Requests Weighted Average Scores",
-        data: entryData.map(a => a.score),
-        fill: false,
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgba(255, 99, 132, 0.2)",
-      },
-    ],
+    const filteredData = data.filter(a => a.created_at !== undefined);
+    const labels = filteredData.map(a => dateToFormattedString(a.created_at as Date));
 
-  };
+    var dataPerEvaluator: { [id: string]: number[]; } = {};
+
+    for(const evaluator of evaluators) {
+        dataPerEvaluator[evaluator.evaluator_id] = Array(labels.length).fill(null);
+    }
+
+    for(const index in data) {
+        const dataEntry = data[index];
+        for(const result of dataEntry.result.results) {
+            dataPerEvaluator[result.evaluator_id][index] = result.normalised_score;
+        }
+    }
+
+    const datasets = 
+    Object.entries(dataPerEvaluator).map(([key, num]) => {
+        console.log("KEY:" + key);
+        return {
+            label: num.filter(x => x !== null).length + " | " + key,
+            data: num,
+            fill: false,
+            spanGaps: true
+        }
+    });
+
+    const plotData = {
+        labels: filteredData.map(a => dateToFormattedString(a.created_at as Date)),
+        datasets: datasets,
+    };
+
+    const options = {
+            plugins: {
+                legend: {
+                    title: {
+                        display: true,
+                        text: "PLACEHOLDER TITLE"
+                    }
+                }
+            }
+    }
 
   return (
     <div>
-      <Line data={data} />
+      <Line data={plotData} options={options} />
     </div>
   );
 }
 
-function extractDataForEvaluator(evaluator_id: string, data: object) {
-
-}
-
-
-function EvaluatorGraph() {
-    const [data, setData] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-
-    useEffect(() => {
-        fetch("/api/results")
-            .then(res => res.json())
-            .then(json => {
-
-                const dataEntries = json.map(toDataEntry).sort((a: DataEntry, b: DataEntry) => (a.timestamp === b.timestamp) ? 0 : (a.timestamp < b.timestamp ? -1 : 1));
-
-                setData(dataEntries);
-                setIsLoaded(true);
-            });
-    }, []);
-
-    if(!isLoaded) {
-        return (
-            <div>
-                <h1>Loading evaluator results...</h1>
-            </div>
-        );
-    }
-
-    console.log(data);
-
+function EvaluatorGraph({data, evaluators}: ChartProps) {
     const n = data.length;
-//    for(let i = 0; i < n; i++) {
-//        const results = data[i]["result"]["results"];
-//        for(const result of results) {
-//            const id = result["evaluator_id"];
-//            const score = result["normalised_score"];
-//        }
-//    }
-
     
     var avg = 0.0;
     for(const el of data) {
-        avg += el.score;
+        if(el.result.weighted_average_score !== undefined)
+            avg += el.result.weighted_average_score;
     }
 
     avg = (n == 0) ? 0.0 : avg / n;
 
-//    avg = Math.floor(avg * 100) / 100.0;
-
     return (
         <div>
-            <ChartData entryData={data}/>
+        <Chart data={data} evaluators={evaluators} />
 
-            <div> Average: {avg.toFixed(2)} </div>
+        <span> Average: {avg.toFixed(2)} </span>
         </div>
     );
 }
@@ -104,63 +101,112 @@ function SingleFilterEvaluator({name, enabledByDefault}: SingleFilterEvaluatorPr
     );
 }
 
+
 interface FiltersProps {
     evaluators: Evaluator[]
 }
-function FilterEvaluator({evaluators}: FiltersProps) {
-    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+function Filters({evaluators}: FiltersProps) {
+    const today = dayjs();
+    const yesterday = today.subtract(1, 'days');
 
-    console.log(evaluators);
-    const evaluatorFilters = evaluators.map(e => 
-        <SingleFilterEvaluator key={e.evaluator_id} name={e.evaluator_id} enabledByDefault={false}/>
-   );
+    const [startDate, setStartDate] = useState<Dayjs>(today);
+    const [endDate, setEndDate] = useState<Dayjs>(yesterday);
+
     return (
-        <div className="relative">
-            <button onClick={() => {
-                setIsDropDownOpen(!isDropDownOpen)
-            }}>Evaluators filter</button>
-
-            { isDropDownOpen &&
-                <div>
-                    <SingleFilterEvaluator name="Aggregated Evaluators" enabledByDefault={true}/>
-                    {evaluatorFilters}
-                </div>
-            }
-
+        <div style={{display: "flex", gap: '2em', padding: '0em 1em 0.5em 1em'}}> 
+            <DateTimePicker
+                label="Start date"
+                value={startDate}
+                onChange={(date) => setStartDate(date != null ? date : today)}
+                slotProps={{
+                  textField: { size: "small" },
+                }}
+              />
+            <DateTimePicker
+                label="End date"
+                value={endDate}
+                onChange={(date) => setEndDate(date != null ? date : yesterday)}
+                slotProps={{
+                  textField: { size: "small" },
+                }}
+              />
         </div>
     );
 }
 
-function Filters({evaluators}: FiltersProps) {
-
-    return (
-        <div> <FilterEvaluator evaluators={evaluators}/> </div>
-    )
-}
-
 export default function Dashboard() {
     const [evaluators, setEvaluators] = useState([]);
+    const [data, setData] = useState<AggregatedResultEntity[]>([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         if(evaluators.length == 0) {
-            fetch("/api/evaluators")
+            fetch("http://localhost:8000/evaluators")
                 .then(res => res.json())
                 .then(json => {
-                    setEvaluators(json.map(toEvaluator))
+                    setEvaluators(json)
+                });
+
+            fetch("http://localhost:8000/results")
+                .then(res => res.json())
+                .then(json => {
+                    json = json as AggregatedResultEntity[];
+                    for(var i = 0; i < json.length; i++) {
+                        json[i].created_at = new Date(json[i].created_at);
+                    }
+                    setData(json);
+                    setIsLoaded(true);
                 });
         } else {
             console.log("Redundant update saved");
         }
-    }, []);
+    }, [isLoaded]);
 
-    console.log(evaluators);
+
+//    console.log(evaluators);
+
+    if(!isLoaded) {
+        return (
+            <div>
+                <h1>Welcome to dashboard</h1>
+                <Filters evaluators={evaluators}/>
+                <h1>Loading results..</h1>
+            </div>
+        );
+    }
 
     return (
             <div>
                 <h1>Welcome to dashboard</h1>
                 <Filters evaluators={evaluators}/>
-
-                <EvaluatorGraph />
+                <EvaluatorGraph data={data} evaluators={evaluators}/>
             </div>
     );
 }
+
+//interface FilterEvaluatorProps {
+//    evaluators: Evaluator[]
+//}
+//function FilterEvaluator({evaluators}: FilterEvaluatorProps) {
+//    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+//
+////    console.log(evaluators);
+//    const evaluatorFilters = evaluators.map(e => 
+//        <SingleFilterEvaluator key={e.evaluator_id} name={e.evaluator_id} enabledByDefault={false}/>
+//   );
+//    return (
+//        <div className="relative">
+//            <button onClick={() => {
+//                setIsDropDownOpen(!isDropDownOpen)
+//            }}>Evaluators filter</button>
+//
+//            { isDropDownOpen &&
+//                <div>
+//                    <SingleFilterEvaluator name="Aggregated Evaluators" enabledByDefault={true}/>
+//                    {evaluatorFilters}
+//                </div>
+//            }
+//
+//        </div>
+//    );
+//}
