d7387649e4f79cc62c4315e9900d8d20ed8d1e0f
Marie Johansen <majoh@itu.dk>
2026-04-25 14:44:55 +0200

    test register method

--- backend/tests/unit/test_registry.py
+++ backend/tests/unit/test_registry.py
@@ -20,3 +20,24 @@ def test_happypath_get_rule_based_evaluator(registry: EvaluationRegistry) -> Non
     evaluator = MockEvaluator()
     registry.register(evaluator.name, evaluator)
     assert registry.get(evaluator.name) is not None
+
+
+def test_happtypath_register(registry: EvaluationRegistry) -> None:
+    registry.register(MockEvaluator().name, MockEvaluator())
+    assert registry.get(MockEvaluator().name) is not None
+
+
+def test_register_fail_same_id(registry: EvaluationRegistry) -> None:
+    evaluator = MockEvaluator()
+    registry.register("id", evaluator)
+    assert registry.register("id", evaluator) is False
+
+
+def test_register_fail_empty_id(registry: EvaluationRegistry) -> None:
+    with pytest.raises(ValueError):
+        registry.register("", MockEvaluator())
+
+
+def test_register_fail_evaluator_none(registry: EvaluationRegistry) -> None:
+    with pytest.raises(ValueError):
+        registry.register("", MockEvaluator())
