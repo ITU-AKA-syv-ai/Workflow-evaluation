1fc90dfbbb4efaabab7e5ad59d692c2d2b573057
Marie Johansen <majoh@itu.dk>
2026-05-04 15:58:11 +0200

    draft insert method

--- backend/app/core/repositories/sqlalchemy_evaluation_repository.py
+++ backend/app/core/repositories/sqlalchemy_evaluation_repository.py
@@ -1,11 +1,20 @@
-﻿from sqlalchemy.orm import Session
+﻿import logging
+from uuid import UUID
 
-from app.core.repositories.i_result_repository import IResultRepository
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+
+from app.core.models.evaluation_model import EvaluationResult
+from app.core.repositories.i_evalution_repository import IEvaluationRepository
+from app.exceptions import ResultPersistenceError
+from app.models import Evaluation
 
 # todo: add insert method that inserts EvaluationResults into the EvaluationResults table and the other queries here
 
+logger = logging.getLogger(__name__)
 
-class SQLAlchemyEvaluationRepository(IResultRepository):
+
+class SQLAlchemyEvaluationRepository(IEvaluationRepository):
     def __init__(self, session: Session) -> None:
         """
         Initialize the ResultRepository with the SQLAlchemy session.
@@ -14,3 +23,24 @@ class SQLAlchemyEvaluationRepository(IResultRepository):
              session (Session): The SQLAlchemy session object.
         """
         self.session = session
+
+    def insert(self, evaluation_result: EvaluationResult, aggregated_result_id: UUID) -> UUID:
+        result = Evaluation(
+            aggregated_result=aggregated_result_id,
+            evaluator_id=evaluation_result.evaluator_id,
+            passed=evaluation_result.passed,
+            reasoning=evaluation_result.reasoning,
+            normalised_score=evaluation_result.normalised_score,
+            execution_time=evaluation_result.execution_time,
+            error=evaluation_result.error,
+        )
+
+        try:
+            self.session.add(result)
+            self.session.commit()
+        except SQLAlchemyError as e:
+            logger.exception("Failed to persist aggregated result")
+            self.session.rollback()
+            raise ResultPersistenceError() from e
+
+        return result.id
