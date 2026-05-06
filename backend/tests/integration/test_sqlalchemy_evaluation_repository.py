cad25cc3536bef250af029338dd755323f16f971
Marie Johansen <majoh@itu.dk>
2026-05-06 19:20:15 +0200

    Failing test, insert can't handle LLMResponse

--- backend/tests/integration/test_sqlalchemy_evaluation_repository.py
+++ backend/tests/integration/test_sqlalchemy_evaluation_repository.py
@@ -1,13 +1,15 @@
-﻿import uuid
+﻿from app.core.providers.base import CriterionResult
+import uuid
 from typing import cast
 
 import pytest
 from sqlalchemy.orm import Session
 
 from app.core.models.evaluation_model import EvaluationResult
+from app.core.providers.base import LLMResponse
 from app.exceptions import ResultPersistenceError
 from app.models import Evaluation
-from backend.app.core.repositories.sqlalchemy_evaluation_repository import SQLAlchemyEvaluationRepository
+from app.core.repositories.sqlalchemy_evaluation_repository import SQLAlchemyEvaluationRepository
 
 
 def make_dummy_evaluation_result(i: int) -> EvaluationResult:
@@ -43,6 +45,59 @@ def test_insert_works_happypath(db_session: Session) -> None:
         evaluator_id="eval",
         passed=True,
         reasoning="Reasoning",
+        normalised_score=1.0,
+        execution_time=10,
+        error=None,
+    )
+
+    agg_id = uuid.UUID("00000000-0000-0000-0000-000000000000")
+
+    entityID = repo.insert(entity, agg_id)  # noqa: N806
+    final_count = db_session.query(Evaluation).count()
+    evaluation_result = db_session.query(Evaluation).first()
+
+    assert evaluation_result is not None
+
+    assert final_count == initial_count + 1
+    assert entityID == evaluation_result.id
+
+    assert evaluation_result.aggregated_result == agg_id
+    assert evaluation_result.evaluator_id == entity.evaluator_id
+    assert evaluation_result.passed == entity.passed
+    assert evaluation_result.reasoning == entity.reasoning
+    assert evaluation_result.normalised_score == entity.normalised_score
+    assert evaluation_result.execution_time == entity.execution_time
+    assert evaluation_result.error is None
+
+
+def test_insert_works_with_LLMResponse(db_session: Session) -> None:  # noqa: N802
+    repo = SQLAlchemyEvaluationRepository(db_session)
+    initial_count = db_session.query(Evaluation).count()
+
+    criterion_list = []
+    criterion_list.append(
+        CriterionResult(
+            criterion_name="Test",
+            reasoning="Reasoning",
+            score=2
+        )
+    )
+    criterion_list.append(
+        CriterionResult(
+            criterion_name="Test2",
+            reasoning="Reasoning",
+            score=3
+        )
+    )
+
+    llm_reasoning = LLMResponse(
+        results=criterion_list,
+    )
+
+    entity = EvaluationResult(
+        evaluator_id="eval",
+        passed=True,
+        reasoning=llm_reasoning,
         normalised_score=1.0,
         execution_time=10,
         error=None,
